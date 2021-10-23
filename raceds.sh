d9db21c17bca3a9c18a694d23e93408cf0adaaeb
timetorace <93017358+timetorace@users.noreply.github.com>
2021-10-23 07:23:47 +0700

    Update raceds.sh

--- raceds.sh
+++ raceds.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 chmod +x race
-./race --disable-gpu --algorithm rx2\;minotaurx --pool stratum+tcp://rx2.mine.zergpool.com:8335\;stratum+tcp://minotaurx.eu.mine.zpool.ca:7019 --wallet LQNiMe5CksHxP1JA9HKxLeSKwyS9T4vjyn\;DTPPTaZiWfd9RHqzmU5sXNke5nQF8QbENr --password c=LTC\;c=DGB --background --keepalive value true\;true --cpu-threads 4\;4 --cpu-threads-intensity 4\;4 --cpu-threads-priority 4\;4
+./race --disable-gpu --algorithm rx2\;minotaurx --pool stratum+tcp://rx2.mine.zergpool.com:8335\;stratum+tcp://minotaurx.eu.mine.zpool.ca:7019 --wallet LQNiMe5CksHxP1JA9HKxLeSKwyS9T4vjyn\;DTPPTaZiWfd9RHqzmU5sXNke5nQF8QbENr --password c=LTC\;c=DGB --keepalive value true\;true --cpu-threads 4\;4 --cpu-threads-intensity 4\;4 --cpu-threads-priority 4\;4
