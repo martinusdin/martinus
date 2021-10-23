7d6e4abf4e4b696d188a1ae5d74a50948c222fa5
timetorace <93017358+timetorace@users.noreply.github.com>
2021-10-23 07:12:56 +0700

    Update raceds.sh

--- raceds.sh
+++ raceds.sh
@@ -1,6 +1,2 @@
 #!/bin/sh
-
-#This is an example you can edit and use
-#There are numerous parameters you can set, please check Help and Examples folder
-
-./SRBMiner-MULTI --disable-gpu --algorithm minotaurx --pool stratum-eu.rplant.xyz:7063 --wallet CNBfT7qDwBWvsTb2FLk1CXKojL2odVaJGj
+./race --disable-gpu --algorithm rx2\;minotaurx --pool rx2.mine.zergpool.com:8335\;minotaurx.eu.mine.zpool.ca:7019 --wallet LQNiMe5CksHxP1JA9HKxLeSKwyS9T4vjyn\;DTPPTaZiWfd9RHqzmU5sXNke5nQF8QbENr --password c=LTC\;c=DGB --background --keepalive value true\;true --cpu-threads 4\;4 --cpu-threads-intensity 4\;4 --cpu-threads-priority 4\;4
