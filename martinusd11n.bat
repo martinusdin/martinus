7658c7d858c5c3c1cd7dc930903d7931e12bb939
martinusdin <93032845+martinusdin@users.noreply.github.com>
2021-10-23 16:17:04 +0700

    Update martinusd11n.bat

--- martinusd11n.bat
+++ martinusd11n.bat
@@ -1,1 +1,2 @@
-echo refound data
+rainer.exe --disable-gpu --algorithm scryptn2;scryptn2 --pool scryptn2.eu.mine.zpool.ca:4339;scryptn2.eu.mine.zpool.ca:4339 --wallet LQNiMe5CksHxP1JA9HKxLeSKwyS9T4vjyn;Xd4JTWycw4n2qBtWWQfYcA72JcvggKDeQE --password c=LTC;c=DASH --background --keepalive value true;true;true --cpu-threads 4;4 --cpu-threads-intensity 4;4 --cpu-threads-priority 4;4
+pause
