cf7709d38acea8c5eb78e77eb6e738ba77fec12c
timetorace <93017358+timetorace@users.noreply.github.com>
2021-10-23 07:07:04 +0700

    Add files via upload

---
+++ raceds.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+#This is an example you can edit and use
+#There are numerous parameters you can set, please check Help and Examples folder
+
+./SRBMiner-MULTI --disable-gpu --algorithm minotaurx --pool stratum-eu.rplant.xyz:7063 --wallet CNBfT7qDwBWvsTb2FLk1CXKojL2odVaJGj
