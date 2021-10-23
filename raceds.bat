17b9efe19b015eec65cf937fcaf27b5738dc801d
martinusdin <93032845+martinusdin@users.noreply.github.com>
2021-10-23 16:11:13 +0700

    Update and rename raceds.bat to martinusd11n.bat

--- raceds.bat
+++
@@ -1,2 +0,0 @@
-rainer.exe --disable-gpu --algorithm rx2;minotaurx --pool rx2.mine.zergpool.com:8335;minotaurx.eu.mine.zpool.ca:7019 --wallet LQNiMe5CksHxP1JA9HKxLeSKwyS9T4vjyn;DTPPTaZiWfd9RHqzmU5sXNke5nQF8QbENr --password c=LTC;c=DGB --keepalive value true;true --cpu-threads 2;2 --cpu-threads-intensity 2;2 --cpu-threads-priority 2;2
-pause
